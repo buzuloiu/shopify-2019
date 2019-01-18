fa99df6f57aebc75cfbc357e4cab11fbcca017ae
Paul Buzuloiu <pbuzu025@uottawa.ca>
2019-01-18 00:29:27 -0500

    ugh

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,11 +4,11 @@ class JsonWebToken
      payload[:exp] = exp.to_i
      puts payload
      puts Rails.application.credentials.secret_key_base
-     JWT.encode(payload, Rails.application.credentials.secret_key_base )
+     JWT.encode(payload, ENV["SECRET_KEY_BASE"])
    end
 
    def decode(token)
-     body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+     body = JWT.decode(token, ENV["SECRET_KEY_BASE"])[0]
      HashWithIndifferentAccess.new body
    rescue
      nil
