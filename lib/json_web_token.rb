2c6a920b9a5b43c3b7c89e372b4bd4669de8f272
Paul Buzuloiu <pbuzu025@uottawa.ca>
2019-01-18 00:01:45 -0500

    debug

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,6 +2,8 @@ class JsonWebToken
  class << self
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
+     puts payload
+     puts Rails.application.credentials.secret_key_base
      JWT.encode(payload, Rails.application.credentials.secret_key_base )
    end
 
