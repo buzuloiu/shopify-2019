ce43a0606396d19cb86e135941f645edcfb8ee8c
Paul Buzuloiu <pbuzu025@uottawa.ca>
2019-01-21 02:49:04 -0500

    docs

--- config/initializers/raddocs.rb
+++ config/initializers/raddocs.rb
@@ -1,3 +1,4 @@
 Raddocs.configure do |config|
   config.docs_dir = "doc/api"
+  config.api_name = "API Docs"
 end
--- config/initializers/rspec_api_documentation.rb
+++ config/initializers/rspec_api_documentation.rb
@@ -7,4 +7,8 @@ RspecApiDocumentation.configure do |config|
   # Possible values are :json, :html, :combined_text, :combined_json,
   #   :json_iodocs, :textile, :markdown, :append_json
   config.format = [:json]
+
+  # Change the name of the API on index pages
+  config.api_name = "API Documentation"
+
 end
